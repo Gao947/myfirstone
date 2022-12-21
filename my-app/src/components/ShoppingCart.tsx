dcb06c0b66de563ee5888ef2820597567ccc3938
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2022-12-21 12:04:13 +0800

    3-12

--- my-app/src/components/ShoppingCart.tsx
+++ my-app/src/components/ShoppingCart.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./ShoppingCart.module.css";
+import { FiShoppingCart } from "react-icons/fi";
 
 interface Props {}
 
@@ -15,16 +16,23 @@ class ShoppingCart extends React.Component<Props, State> {
     };
   }
 
+  handleClick = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log("e.target ", e.target)
+    console.log("e.currentTarget ", e.currentTarget)
+    if((e.target as HTMLElement).nodeName === "SPAN"){
+      this.setState({ isOpen: !this.state.isOpen });
+    }
+  }
+
   render() {
     return (
       <div className={styles.cartContainer}>
         <button
           className={styles.button}
-          onClick={() => {
-            this.setState({ isOpen: !this.state.isOpen });
-          }}
+          onClick={this.handleClick}
         >
-          购物车 2 (件)
+          <FiShoppingCart />
+          <span>ShoppingCart 2</span> 
         </button>
         <div
           className={styles.cartDropDown}
